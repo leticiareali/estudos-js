c562b48d069f826378fa83d6e6a70875f06d2c52
leticiareali <realileticia@gmail.com>
2022-02-13 18:56:52 -0300

    ex da aula1

---
+++ type.js
@@ -0,0 +1,57 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante (decimais)
+
+const numeroPontoFlutuante = 3.3;
+const pontoFLutuanteSemZero = .5
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+// NaN = Not a Number
+
+const alura = "Alura";
+console.log(alura * primeiroNumero);
+
+const n1 = 10
+const n2 = 0
+console.log (n1 / n2);
+
+// strings
+
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "senhaSuperSegura123!";
+const stringNumeros = "1234";
+
+const citacao = 'o Leo disse: "Oi"';
+const citacao1 = "Meu nome é "
+const meuNome = "Leonardo";
+console.log(citacao);
+
+// concatenação (+)
+console.log(citacao1 + meuNome);
+
+// qtd de caracteres 
+const senha2 = "minhaSenha123";
+console.log(senha2.length); // 13 caracteres
+
+// boolean  (true or false)
+
+const primeiroNumero1 = 5;
+const segundoNumero1= 5;
+console.log(primeiroNumero1 === segundoNumero1); //comparação
+
+const text1 = "Alura";
+const text2 = "alura";
+console.log(text1 === text2);

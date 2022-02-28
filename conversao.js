bd49c834da4e772f264064060c95bf37204d5dce
leticiareali <realileticia@gmail.com>
2022-02-28 14:12:11 -0300

    codigos var e conversoes

---
+++ conversao.js
@@ -0,0 +1,19 @@
+// tipo de dado
+// booleanos
+
+// conversao implicita - permite que converta um tipo de dado em outro
+
+const numero = 456;
+const numeroString = "456";
+console.log (numero == numeroString)
+
+console.log(numero + numeroString)
+
+// == substitui o tipo
+// === compara os tipos
+
+// conversao explicita
+//Number()
+//String()
+
+console.log(numero + Number(numeroString))
---
+++ var.js
@@ -0,0 +1,39 @@
+// //var
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area)
+// var area
+
+//let - permite alterar os valores
+
+
+// let forma = 'retangulo'
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if(forma === 'retangulo'){
+//     area = altura * comprimento;
+// } else{
+//     area = (altura * comprimento) / 2;
+// }
+// console.log(area)
+// let area;
+
+// const - constante, não alterável
+
+const forma = 'triangulo';
+const altura = 5;
+const comprimento = 7;
+let area; // toda const tem que ter um valor atribuido;
+            // nesse caso deveria ser let area, pois iremos alterar o valor;
+if(forma === 'quadrado'){
+    area = altura * comprimento;
+} else {(altura * comprimento) / 2;
+}
+
+console.log(area)
+
